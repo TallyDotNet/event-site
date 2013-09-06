d81dcffd2e711471da2f2e0b067e555552c6753d
EisenbergEffect <rob@bluespire.com>
2013-09-05 22:24:59 -0400

    Bye bye old stuff....

--- src/Castra.Web/Queries/RandomFeaturedSponsor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Castra.Web.Queries
-{
-	using BlueSpire.Kernel.Data;
-	using Models;
-
-	public class RandomFeaturedSponsor : IQuery<Sponsor>
-	{
-	}
-}
--- src/Castra.Web/Queries/RegistrationStats.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Castra.Web.Queries
-{
-	using System.Collections.Generic;
-	using BlueSpire.Kernel.Data;
-
-	public class RegistrationStats : IQuery<Dictionary<string, long>>
-	{
-	}
-}
