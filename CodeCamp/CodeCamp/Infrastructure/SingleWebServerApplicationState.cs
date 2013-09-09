72523676ada02ec816b6ba9f57b7d57c9f8a511b
EisenbergEffect <rob@bluespire.com>
2013-09-09 01:42:42 -0400

    Setting up login/logout and session data.

--- CodeCamp/CodeCamp/Infrastructure/SingleWebServerApplicationState.cs
+++ CodeCamp/CodeCamp/Infrastructure/SingleWebServerApplicationState.cs
@@ -1,18 +1,55 @@
 ﻿using System.Configuration;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Security;
 using CodeCamp.Domain;
 using CodeCamp.Domain.Model;
 using Raven.Client;
 
 namespace CodeCamp.Infrastructure {
     public class SingleWebServerApplicationState : IApplicationState {
-        IDocumentSession docSession;
+        const string CurrentUserKey = "CurrentUserKey";
 
-        public SingleWebServerApplicationState(IDocumentSession docSession) {
+        readonly IDocumentSession docSession;
+        readonly HttpContextBase httpContext;
+
+        public SingleWebServerApplicationState(IDocumentSession docSession, HttpContextBase httpContext) {
             this.docSession = docSession;
+            this.httpContext = httpContext;
         }
 
         public User User {
-            get { return null; }
+            get {
+                try {
+                    var user = httpContext.Session[CurrentUserKey] as User;
+                    if(user != null) {
+                        return user;
+                    }
+
+                    var authCookie = httpContext.Request.Cookies[FormsAuthentication.FormsCookieName];
+                    if(authCookie == null || string.IsNullOrWhiteSpace(authCookie.Value)) {
+                        return null;
+                    }
+
+                    var authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                    var userId = authTicket.UserData;
+
+                    if(string.IsNullOrEmpty(userId)) {
+                        return null;
+                    }
+
+                    user = docSession.Load<User>(userId);
+
+                    if(user != null) {
+                        SetUser(user);
+                    }
+
+                    return user;
+                } catch {
+                    return null;
+                }
+            }
         }
 
         public ScheduledEvent UpcomingEvent { get; private set; }
@@ -22,11 +59,24 @@ namespace CodeCamp.Infrastructure {
         }
 
         public void Login(User user, bool persist) {
-            throw new System.NotImplementedException();
+            SetUser(user);
+            FormsAuthentication.SetAuthCookie(user.Id, persist);
         }
 
         public void Logout() {
-            throw new System.NotImplementedException();
+            httpContext.Session.Clear();
+            httpContext.Session.Abandon();
+            FormsAuthentication.SignOut();
+        }
+
+        void SetUser(User user) {
+            httpContext.Session[CurrentUserKey] = user;
+
+            if(httpContext.User != null && httpContext.User.Identity.Name == user.Username) {
+                return;
+            }
+
+            httpContext.User = new GenericPrincipal(new GenericIdentity(user.Username), user.Roles.ToArray());
         }
     }
 }
