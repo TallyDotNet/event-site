8fbb5087ea4dfb3b69db8af7d9f6570f29db3fb6
EisenbergEffect <rob@bluespire.com>
2013-09-15 15:52:49 -0400

    Added parking instructions.

--- EventSite/Domain/Model/Venue.cs
+++ EventSite/Domain/Model/Venue.cs
@@ -7,5 +7,6 @@
         public string StateOrProvince { get; set; }
         public string Country { get; set; }
         public string PostalCode { get; set; }
+        public string ParkingInformation { get; set; }
     }
 }
