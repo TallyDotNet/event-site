876bbd765cef7d8295f703a905a0dfbe2d8ce1e4
EisenbergEffect <rob@bluespire.com>
2013-09-25 08:47:01 -0400

    Some tweaks to merge of session status change.
    -Also updated vendor photo.

--- EventSite/Domain/Commands/ChangeSessionStatus.cs
+++ EventSite/Domain/Commands/ChangeSessionStatus.cs
@@ -22,27 +22,27 @@ namespace EventSite.Domain.Commands {
 
             session.Status = status;
 
-            if (status == SessionStatus.Approved) {
-                SetSpeakerStatus(session.Event.Id, session.User.Id, true);
-            }
-            else {
+            if(status == SessionStatus.Approved) {
+                setSpeakerStatus(session.Event.Id, session.User.Id, true);
+            } else {
                 var sessionsForSpeaker = Bus.Query(new SubmittedSessions(session.Event.Id, session.User.Id));
-                if (sessionsForSpeaker.All(s => s.Status != SessionStatus.Approved)) {
-                    SetSpeakerStatus(session.Event.Id, session.User.Id, false);
+                if(sessionsForSpeaker.All(s => s.Status != SessionStatus.Approved)) {
+                    setSpeakerStatus(session.Event.Id, session.User.Id, false);
                 }
             }
-            
+
+            var message = string.Format("The session \"{0}\" has been given a status of {1}.", session.Name, status);
 
             if(status == SessionStatus.Approved || status == SessionStatus.Rejected) {
                 var user = DocSession.Load<User>(session.User.Id);
                 Bus.Enqueue(new SendSessionStatusChangeEmail(State.CurrentEvent, session, user));
+                message += " An email has been sent to the submitter to inform them.";
             }
 
-            return SuccessFormat("The session \"{0}\" has been given a status of {1}.", session.Name, status);
+            return Success(message);
         }
 
-        private void SetSpeakerStatus(string eventId, string userId, bool isSpeaker)
-        {
+        void setSpeakerStatus(string eventId, string userId, bool isSpeaker) {
             var reg = Bus.Query(new GetUserRegistration(eventId, userId));
             reg.IsSpeaker = isSpeaker;
         }
