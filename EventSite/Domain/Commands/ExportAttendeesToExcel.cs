a4d19d69afa9c5555644ca6b0a24bf2ea70957f5
Jesse Taber <jesse.taber@gmail.com>
2013-10-12 12:15:06 -0400

    Adding ref to EPPlus library and the beginnings of a command for building excel files.

---
+++ EventSite/Domain/Commands/ExportAttendeesToExcel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using EventSite.Domain.Infrastructure;
+using OfficeOpenXml;
+
+namespace EventSite.Domain.Commands
+{
+    public abstract class ExportToExcel<T> : Work {
+        protected abstract IEnumerable<T> DataSource { get; }
+
+        protected abstract IDictionary<string, Func<string>> Columns { get; }
+
+        protected abstract string TargetFileName { get; }
+
+        public override void Process() {
+            var excelFile = new FileInfo(TargetFileName);
+
+            using (var package = new ExcelPackage(excelFile))
+            {
+                var ws = package.Workbook.Worksheets.Add("Attendees");
+                ws.View.ShowGridLines = true;
+
+                var columnCounter = 0;
+                foreach (var column in Columns)
+                {
+                    ws.Cells[columnCounter, 1].Value = column.Key;
+                    columnCounter++;
+                }
+
+                package.Save();
+            }   
+        }
+    }
+}
