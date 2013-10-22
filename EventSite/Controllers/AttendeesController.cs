4e379f4f86b646019990e9da25fa07c04a0da43c
EisenbergEffect <rob@bluespire.com>
2013-10-21 20:13:30 -0400

    A few minor tweaks.

--- EventSite/Controllers/AttendeesController.cs
+++ EventSite/Controllers/AttendeesController.cs
@@ -1,21 +1,17 @@
-﻿using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using EventSite.Domain;
 using EventSite.Domain.Model;
 using EventSite.Domain.Queries;
-using EventSite.ViewModels.RegisteredUsers;
 using EventSite.Infrastructure.Controllers;
+using EventSite.ViewModels.RegisteredUsers;
 
-namespace EventSite.Controllers
-{
+namespace EventSite.Controllers {
     public class AttendeesController : BaseController {
-        private const int PageSize = 24; //we show six per row, so we'll show 6 rows
+        const int PageSize = 24; //we show six per row, so we'll show 4 rows
 
         [HttpGet]
-        public ActionResult Index(string eventSlug = null, int page = 1)
-        {
-            if (string.IsNullOrEmpty(eventSlug) && State.NoEventScheduled())
-            {
+        public ActionResult Index(string eventSlug = null, int page = 1) {
+            if(string.IsNullOrEmpty(eventSlug) && State.NoEventScheduled()) {
                 return View("NoEventScheduled");
             }
 
