91d169c8a98e944dd42dc629ffd7bf94c9962163
Jesse Taber <jesse.taber@gmail.com>
2014-07-09 20:37:20 -0400

    Updating URL building logic to take config settings into account.

---
+++ EventSite/ViewModels/Account/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EventSite.ViewModels.Account {
+    public class LoginViewModel {
+        public string GoogleAuthUrl { get; set; }
+        public string FacebookAuthUrl { get; set; }
+        public string TwitterAuthUrl { get; set; }
+        public string GithubAuthUrl { get; set; }
+        public string LinkedInAuthUrl { get; set; }
+        public string WindowLiveAuthUrl { get; set; }
+    }
+}
